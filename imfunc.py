fcaa8a13c109053e54e39521dfa735d520ba31ae
Divanchik <d-sh-2016@list.ru>
2021-12-26 12:59:08 +0400

    Добавление гистограммы и финальные правки.

--- imfunc.py
+++ imfunc.py
@@ -1,20 +1,39 @@
-from PIL import Image
+from PIL import Image, ImageTk
 from imagehash import average_hash, phash
 import numpy as np
 import os
+from tqdm import tqdm
+
+
+def r(c: list) -> int:
+    '''
+    Значение R.
+    '''
+    return c[0]
+
+
+def g(c: list) -> int:
+    '''
+    Значение G.
+    '''
+    return c[1]
+
+
+def b(c: list) -> int:
+    '''
+    Значение B.
+    '''
+    return c[2]
 
 
 def supported(format: list, path: str):
+    '''
+    Проверка, принадлежит ли формат изображения `path` списку `list`.
+    '''
     for i in format:
         if path.endswith(i):
             return True
     return False
-
-def assemble(name, path, length):
-    path = "| " + path
-    nl = len(name)
-    pl = len(path)
-    return name + ' '*(length-nl-pl) + path
 
 
 def greyscale_PAL(c: list) -> int:
@@ -24,74 +43,135 @@ def greyscale_PAL(c: list) -> int:
     return int(0.299*c[0] + 0.587*c[1] + 0.114*c[2])
 
 
-def greyscale_HDTV(c: list) -> int:
-    '''
-    RGB в оттенок серого.
-    '''
-    return int(0.2126*c[0] + 0.7152*c[1] + 0.0722*c[2])
-
-
 def scan(path: str, format: list):
+    '''
+    Сканирование папки(не рекурсивно).
+    Возвращает словарь вида { name: path, ... }.
+    '''
     res = {}
-    for name in os.listdir(path):
+    for name in tqdm(os.listdir(path), desc="Сканируем", ncols=100):
         if supported(format, name):
             p = os.path.join(path, name)
-            res[name] = [p]
+            res[name] = []
+            res[name].append(p)
     return res.copy()
 
 
 def scan_rec(path: str, format: list):
+    '''
+    Сканирование папки(рекурсивно).
+    Возвращает словарь вида { name: path, ... }.
+    '''
     res = {}
-    for folder in list(os.walk(path)):
+    for folder in tqdm(list(os.walk(path)), desc="Сканируем", ncols=100):
         for name in folder[2]:
             if supported(format, name):
                 p = os.path.join(folder[0], name)
-                res[name] = [p]
+                res[name] = []
+                res[name].append(p)
     return res.copy()
 
 
-def sko(image, size=64, func=greyscale_PAL, prec=3) -> float:
-    img = image.resize((size, size))
-    values = [func(i) for i in img.getdata()]
-    n = len(values)
-    res = np.sqrt(n/(n-1)*np.std(values)**2)
+def hist(image):
+    try:
+        clrs = image.getcolors(image.size[0]*image.size[1])
+        r_c = [0 for i in range(256)]
+        g_c = r_c.copy()
+        b_c = r_c.copy()
+        for n, c in clrs:
+            r_c[r(c)] += n
+            g_c[g(c)] += n
+            b_c[b(c)] += n
+        arr = [round((r_c[i] + g_c[i] + b_c[i])/3, 3) for i in range(256)]
+        return arr.copy()
+    except TypeError:
+        return -1
+    except IndexError:
+        return -2
+
+
+def cmp_hist(base, other):
+    tmp = np.abs(np.subtract(base, other))
+    return np.std(tmp)
+
+
+def sko(image, size=64, prec=3) -> float:
+    '''
+    Принимает изображение `image` и вычисляет среднее квадратичное отклонение.
+    '''
+    try:
+        img = image.resize((size, size))
+        values = [greyscale_PAL(i) for i in img.getdata()]
+        n = len(values)
+        res = np.sqrt(n/(n-1)*np.std(values)**2)
+    except TypeError:
+        res = -1
+    except IndexError:
+        res = -2
     return round(res, prec)
 
 
 def cmp_sko(target: float, other: float) -> float:
+    '''
+    Сравнение двух СКО.
+    Возвращает, на сколько процентов `other` отличается от `target`.
+    '''
     return np.abs(1 - other/target)*100
 
 
 class scanner:
     def __init__(self, target_path: str, folder_path: str, formats: list):
         self.folder = folder_path
-        self.image = target_path
+        self.target = target_path
         self.format = formats.copy()
         self.info = {}
     
     def scan(self, rec: bool):
+        '''
+        Сканирование папки и сохранение списка найденных изображений.
+        '''
         if rec:
             self.info = scan_rec(self.folder, self.format)
         else:
             self.info = scan(self.folder, self.format)
-        # for i in self.info.keys():
-        #     print(i)
     
     def calc(self, func):
-        for name, info in self.info.items():
+        '''
+        Вычисление хэшей изображений и их сохранение.
+        '''
+        e = []
+        for name, info in tqdm(self.info.items(), desc="Вычисление цифровых отпечатков", ncols=100):
             img = Image.open(info[0])
-            self.info[name].append(func(img))
+            tmp_val = func(img)
+            if tmp_val == -1:
+                e.append(info[0] + " >> неверные данные!")
+            elif tmp_val == -2:
+                e.append(info[0] + " >> проблемы с индексацией!")
+            else:
+                self.info[name].append(func(img))
+        for i in e:
+            print(i)
+        for n in self.info.keys():
+            print(n, self.info[n])
     
     def find(self, func, dist=5, rec=False):
+        '''
+        Сканирование, вычисление хэшей и отбор изображений по тому, как сильно они отличаются от образца.
+        Возвращает список типа [ [ `name`, `path` ], ... ].
+        '''
         self.scan(rec)
         self.calc(func)
-        target = func(Image.open(self.image))
+        target = func(Image.open(self.target))
         res = []
         for name, info in self.info.items():
-            if func==sko:
-                if cmp_sko(target, info[1]) < dist:
-                    res.append([name, info[0]])
-            else:
-                if target - info[1] < dist:
-                    res.append([name, info[0]])
+            if len(info) == 2:
+                if func==sko:
+                    if cmp_sko(target, info[1]) < dist:
+                        res.append({'name': name, 'path': info[0], 'rate': str(round(cmp_sko(target, info[1]), 3))})
+                elif func==hist:
+                    if cmp_hist(target, info[1]) < dist*500:
+                        res.append({'name': name, 'path': info[0], 'rate': str(round(cmp_hist(target, info[1]), 3))})
+                else:
+                    if target - info[1] < dist:
+                        res.append({'name': name, 'path': info[0], 'rate': str(target-info[1])})
         return res.copy()
--- main.py
+++ main.py
@@ -1,88 +1,81 @@
-# from numpy.core.fromnumeric import var
-from imfunc import assemble, scanner, sko, average_hash, phash
+from imfunc import scanner, sko, average_hash, phash, hist, tqdm
 from tkinter import *
 from tkinter.filedialog import *
 from tkinter.messagebox import *
-# from tkinter.ttk import *
+from PIL import Image, ImageTk
 APP_WIDTH = 1000
-APP_HEIGHT = 550
+APP_HEIGHT = 700
 
 
 # главное окно
 root = Tk()
 root.title('Duplicate finder')
-root.resizable(False, False)
-root.geometry('{}x{}+{}+{}'.format(APP_WIDTH, APP_HEIGHT, (root.winfo_screenwidth() -
-              APP_WIDTH)//2, (root.winfo_screenheight() - APP_HEIGHT)//2))
+root.geometry('{}x{}+{}+{}'.format(APP_WIDTH, APP_HEIGHT, (root.winfo_screenwidth() - APP_WIDTH)//2, (root.winfo_screenheight() - APP_HEIGHT)//2))
 
 
 # переменные
-target_base = "Путь к изображению"
-folder_base = "Путь к папке"
-target_path = StringVar(value="")
-folder_path = StringVar(value="")
+label_target = "Путь к изображению"
+label_folder = "Путь к папке"
+path_target = StringVar(value="")
+path_folder = StringVar(value="")
 
 format = {}
 format['.PNG'] = BooleanVar(value=True)
 format['.JPEG'] = BooleanVar(value=True)
 
-modelist = [sko, average_hash, phash]
-mode = IntVar(value=0)
-modedist = IntVar(value=0)
+resimg = 0
+
+mode = [sko, average_hash, phash, hist]
+mode_index = IntVar(value=0)
+distance = IntVar(value=0)
 files = []
-
-scan_rec = BooleanVar(value=False)
-
-menu_main = Menu(root)                 # главное меню
-menu_file = Menu(menu_main, tearoff=0) # меню "Файл"
-
-menu_file.add_command(label="Открыть...")
-menu_file.add_command(label="Сохранить...")
-menu_file.add_command(label="Выход")
-
-menu_main.add_cascade(label="Файл", menu=menu_file)
-menu_main.add_command(label="О программе")
+is_recursive = BooleanVar(value=True)
 
 
 # фреймы
-frm_actions = Frame(root, relief=RAISED)
+frm_actions = Frame(root, relief=FLAT)
 frm_options = LabelFrame(root, relief=GROOVE, text="Настройки")
 frm_files = Frame(root, relief=FLAT)
+frm_demo = Frame(root, relief=RAISED)
 
 
 # действия
 btn_scan = Button(frm_actions, text="Сканировать")
-btn_delete = Button(frm_actions, text="Удалить")
+btn_show = Button(frm_actions, text="Показать")
 
 
 # настройки
-lbl_folder = Label(frm_options, text=folder_base, justify=LEFT, anchor=W)
-btn_bfolder = Button(frm_options, text="Открыть")
-lbl_target = Label(frm_options, text=target_base, justify=LEFT, anchor=W)
-btn_btarget = Button(frm_options, text="Открыть")
-lbl_format = Label(frm_options, text="Расширение:", justify=LEFT, anchor=W)
-cbt_jpeg = Checkbutton(frm_options, text="*.JPEG",
-                       variable=format[".JPEG"], onvalue=True, offvalue=False)
-cbt_png = Checkbutton(frm_options, text="*.PNG",
-                      variable=format[".PNG"], onvalue=True, offvalue=False)
+lbl_folder = Label(frm_options, text=label_folder, justify=LEFT, anchor=W)
+btn_folder = Button(frm_options, text="Открыть")
+lbl_target = Label(frm_options, text=label_target, justify=LEFT, anchor=W)
+btn_target = Button(frm_options, text="Открыть")
 
-lbl_method = Label(frm_options, text="Метод сравнения:", justify=LEFT, anchor=W)
-rbt_sko = Radiobutton(frm_options, text="СКО", variable=mode, value=0)
-rbt_ahash = Radiobutton(frm_options, text="Average hash", variable=mode, value=1)
-rbt_phash = Radiobutton(frm_options, text="Perceptive hash", variable=mode, value=2)
-scl_dist = Scale(frm_options, orient=HORIZONTAL, from_=0, to=20, variable=modedist, showvalue=True)
-cbt_rec = Checkbutton(frm_options, text="Сканировать рекурсивно", justify=LEFT, anchor=W, variable=scan_rec, onvalue=True, offvalue=False)
+lbl_format = Label(frm_options, text="Расширение:", justify=LEFT, anchor=W)
+cbt_jpeg = Checkbutton(frm_options, text="*.JPEG", variable=format[".JPEG"], onvalue=True, offvalue=False)
+cbt_png = Checkbutton(frm_options, text="*.PNG", variable=format[".PNG"], onvalue=True, offvalue=False)
+
+lbl_mode = Label(frm_options, text="Метод сравнения:", justify=LEFT, anchor=W)
+rbt_sko = Radiobutton(frm_options, text="СКО", variable=mode_index, value=0)
+rbt_ahash = Radiobutton(frm_options, text="Средний хэш", variable=mode_index, value=1)
+rbt_phash = Radiobutton(frm_options, text="Перцептивный хэш", variable=mode_index, value=2)
+rbt_hist = Radiobutton(frm_options, text="Гистограмма", variable=mode_index, value=3)
+
+lbl_dist = Label(frm_options, text="Максимальное расстояние:", justify=LEFT, anchor=W)
+scl_dist = Scale(frm_options, orient=HORIZONTAL, from_=0, to=20, variable=distance, showvalue=True)
+cbt_rec = Checkbutton(frm_options, text="Рекурсивное сканирование", justify=LEFT, anchor=W, variable=is_recursive, onvalue=True, offvalue=False)
 
 
 # файлы
-list_files = Listbox(frm_files, bd=0, bg='white',
-                     fg='black', justify='left', width=100)
+list_files = Listbox(frm_files, bd=0, bg='white', fg='black', justify='left', width=50, selectmode=BROWSE)
 scr_files = Scrollbar(frm_files, command=list_files.yview)
 list_files.config(yscrollcommand=scr_files.set)
 
+# демо
+lbl_img = Label(frm_demo)
+
 def get_folder():
-    folder_path.set(askdirectory(title="Выберите папку для сканирования"))
-    lbl_folder.config(text=folder_base + ":\n" + folder_path.get())
+    path_folder.set(askdirectory(title="Выберите папку для сканирования"))
+    lbl_folder.config(text=label_folder + ":\n" + path_folder.get())
 
 def get_target():
     ft = (
@@ -91,49 +84,75 @@ def get_target():
         ('JPEG', '*.jpeg'),
         ('All files', '*.*')
     )
-    target_path.set(askopenfilename(title="Выберите изображение образец", filetypes=ft))
-    lbl_target.config(text=target_base + ":\n" + target_path.get())
+    path_target.set(askopenfilename(title="Выберите изображение образец", filetypes=ft))
+    lbl_target.config(text=label_target + ":\n" + path_target.get())
+
 def find_all():
     global files
     formats = []
     if format[".PNG"]:
         formats.append('.png')
+        formats.append('.PNG')
     if format[".JPEG"]:
         formats.append('.jpeg')
+        formats.append('.JPEG')
         formats.append('.jpg')
         formats.append('.JPG')
-    a = scanner(target_path.get(), folder_path.get(), formats)
-    files = a.find(modelist[mode.get()], modedist.get(), scan_rec.get())
+    a = scanner(path_target.get(), path_folder.get(), formats)
+    files = a.find(mode[mode_index.get()], distance.get(), is_recursive.get())
     list_files.delete(0, list_files.size()-1)
+    for i in tqdm(range(len(files)-1), desc="Сортировка", ncols=100):
+        for j in range(len(files)-2):
+            if files[j]['rate'] > files[j+1]['rate']:
+                files[j], files[j+1] = files[j+1], files[j]
     for i in files:
-        list_files.insert(END, assemble(i[0], i[1], 100))
+        list_files.insert(END, i['name']+" "*(50-len(i['rate'])-len(i['name']))+i['rate'])
     showinfo(title="Информация", message="{} изображений найдено!".format(len(files)))
-btn_bfolder.config(command=get_folder)
-btn_btarget.config(command=get_target)
+
+def show_selected():
+    global resimg
+    maxsize = 500
+    try:
+        ind = list_files.curselection()[0]
+        img = Image.open(files[ind]['path'])
+        resize_ratio = maxsize/max(img.size)
+        newsize = (int(img.width*resize_ratio), int(img.height*resize_ratio))
+        print(img.size, "->", newsize)
+        resimg = ImageTk.PhotoImage(img.resize(newsize))
+        lbl_img.config(image=resimg)
+    except IndexError:
+        print("Ничего не выбрано!")
+
+btn_folder.config(command=get_folder)
+btn_target.config(command=get_target)
 btn_scan.config(command=find_all)
+btn_show.config(command=show_selected)
 
 
 # упаковка
-root.config(menu=menu_main)
+frm_actions.pack(side=BOTTOM, fill=X)
+frm_options.pack(side=LEFT, fill=Y, padx=5, pady=5)
+frm_files.pack(side=LEFT, fill=Y, padx=5, pady=5)
+frm_demo.pack(side=LEFT, fill=Y, padx=5, pady=5, expand=1)
 
-frm_actions.pack(side=TOP, fill=X)
-frm_options.pack(side=LEFT, fill=BOTH, padx=5, pady=5)
-frm_files.pack(side=RIGHT,fill=Y, padx=5, pady=5, expand=1)
+lbl_img.pack(expand=1)
 
 lbl_folder.pack(side=TOP, fill=X, padx=5, pady=5)
-btn_bfolder.pack(side=TOP, padx=5, pady=5, anchor=E)
+btn_folder.pack(side=TOP, padx=5, pady=5, anchor=E)
 lbl_target.pack(side=TOP, fill=X, padx=5, pady=5)
-btn_btarget.pack(side=TOP, padx=5, pady=5, anchor=E)
+btn_target.pack(side=TOP, padx=5, pady=5, anchor=E)
 
 lbl_format.pack(side=TOP, fill=X, padx=5, pady=5)
 cbt_jpeg.pack(side=TOP, padx=5, pady=5, anchor=W)
 cbt_png.pack(side=TOP, padx=5, pady=5, anchor=W)
 
-lbl_method.pack(side=TOP, fill=X, padx=5, pady=5)
+lbl_mode.pack(side=TOP, fill=X, padx=5, pady=5)
 rbt_sko.pack(side=TOP, padx=5, pady=5, anchor=W)
 rbt_ahash.pack(side=TOP, padx=5, pady=5, anchor=W)
 rbt_phash.pack(side=TOP, padx=5, pady=5, anchor=W)
+rbt_hist.pack(side=TOP, padx=5, pady=5, anchor=W)
 
+lbl_dist.pack(side=TOP, fill=X, padx=5, pady=5)
 scl_dist.pack(side=TOP, fill=X, padx=5, pady=5)
 cbt_rec.pack(side=TOP, padx=5, pady=5, anchor=W)
 
@@ -141,7 +160,7 @@ scr_files.pack(side=RIGHT, fill=Y)
 list_files.pack(side=LEFT, fill=BOTH)
 
 btn_scan.pack(side=LEFT, padx=5, pady=5)
-btn_delete.pack(side=LEFT, padx=5, pady=5)
+btn_show.pack(side=RIGHT, padx=5, pady=5)
 
 
 # run the application
